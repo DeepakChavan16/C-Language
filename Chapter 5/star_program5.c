e0590d2b7e72491f37e6b9863dacea26db2c8390
Deepak Chavan <deepakchavan4235@gmail.com>
2026-09-09 22:30:21 +0530

    Add files via upload

---
+++ Chapter 5/star_program5.c
@@ -0,0 +1,18 @@
+//Write a program to show star pattern
+
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the number of starts you want :");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j=0; j<2*i+1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+return 0;
+}
