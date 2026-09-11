93d3492f06a15faf83ef2eebd525d9b14e352a5d
Deepak <deepakchavan4235@gmail.com>
2026-09-11 17:15:20 +0530

    Uploding for trial

---
+++ Chapter 7 arrays/01_arrays.c
@@ -0,0 +1,13 @@
+//An array is a collection of similar elements. Array allows a single
+//variable to store multiple values.
+
+#include <stdio.h>
+
+int main(){
+    int marks[90];
+    
+    marks[0] = 45;
+    marks[1] = 94;
+    printf("Marks 0 and Marks 1 is %d %d", marks[0], marks[1]);
+return 0;
+}
